fdcbe2b9d53c53dc12591d06f1a607ce92239885
thiagopedro99 <thiago.gefa@gmail.com>
2024-04-21 14:55:14 -0300

    Atualização index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 import * as functions from "./functions.js"
 import { validacao,validacaoEmail,validacaoEstado,obj,envioForm } from "./formContato.js";
-import { modal } from "./register-modal.js";
 
 functions.carouselImg();
 functions.destaques();
